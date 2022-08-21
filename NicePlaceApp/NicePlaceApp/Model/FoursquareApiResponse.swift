def912e02cd625858788842db90c3a90ef70138b
Camila Luísa Farias <camila.luisafarias@thoughtworks.com>
2022-08-21 15:10:44 -0300

    Setup FoursquareApiResponse

--- NicePlaceApp/NicePlaceApp/Model/FoursquareApiResponse.swift
+++ NicePlaceApp/NicePlaceApp/Model/FoursquareApiResponse.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct FoursquareApiResponse: Codable {
+    let results: [FoursquareResult]
+    
+    enum CodingKeys: String, CodingKey {
+        case results = "results"
+    }
+}
+
+struct FoursquareResult: Codable {
+    let name: String
+
+    enum CodingKeys: String, CodingKey {
+        case name
+    }
+}
