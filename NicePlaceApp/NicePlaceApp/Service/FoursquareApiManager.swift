9f7bdda910c9663448b461cba9a58de92fdac1ec
Camila Luísa Farias <camila.luisafarias@thoughtworks.com>
2022-08-21 15:30:19 -0300

    Setup FoursquareApiManager and FoursquareServiceTests

--- NicePlaceApp/NicePlaceApp/Service/FoursquareApiManager.swift
+++ NicePlaceApp/NicePlaceApp/Service/FoursquareApiManager.swift
@@ -6,3 +6,90 @@
 //
 
 import Foundation
+
+protocol URLSessionProtocol {
+    func dataTask(urlRequest: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
+}
+
+protocol URLSessionDataTaskProtocol { func resume() }
+
+extension URLSessionDataTask: URLSessionDataTaskProtocol {}
+extension URLSession: URLSessionProtocol {
+    func dataTask(urlRequest: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+        return dataTask(with: urlRequest, completionHandler: completionHandler) as URLSessionDataTaskProtocol
+    }
+}
+
+protocol FoursquareApiManagerProtocol {
+    func placeSeach(placeName: String, latitude: Double, longitude: Double, completion: @escaping (Result<FoursquareApiResponse, NetworkError>) -> Void)
+}
+
+class FoursquareApiManager: FoursquareApiManagerProtocol {
+    private let session: URLSessionProtocol
+    private let dispatchQueueWrapper: DispatchQueueWrapperProtocol
+
+    init(
+        session: URLSessionProtocol = URLSession.shared,
+         dispatchQueueWrapper: DispatchQueueWrapperProtocol = DispatchQueueWrapper()
+    ) {
+        self.session = session
+        self.dispatchQueueWrapper = dispatchQueueWrapper
+    }
+
+    func placeSeach(placeName: String, latitude: Double, longitude: Double, completion: @escaping (Result<FoursquareApiResponse, NetworkError>) -> Void) {
+
+        var components = makeFoursquareApiComoponents()
+        components.queryItems = [
+            URLQueryItem(name: "query", value: "\(placeName)"),
+            URLQueryItem(name: "ll", value: "\(latitude) & \(longitude)")
+        ]
+
+        guard let url = components.url else {
+            completion(.failure(.invalidUrl))
+            return
+        }
+
+        let urlRequest = URLRequest(url: url, timeoutInterval: 10)
+
+        let task = session.dataTask(urlRequest: urlRequest) { [weak self] data, urlResponse, error in
+
+            self?.dispatchQueueWrapper.mainAsync {
+
+                if error != nil {
+                    completion(.failure(.failed(error: error!)))
+                } else if let data = data {
+                    do {
+                        let decoder = JSONDecoder()
+                        let response = try decoder.decode(FoursquareApiResponse.self, from: data)
+
+                        completion(.success(response))
+                    } catch {
+                        completion(.failure(.errorDecode))
+                    }
+                } else {
+                    print("unknown dataTask error")
+                    completion(.failure(.unknownError))
+                }
+            }
+        }
+
+        task.resume()
+    }
+}
+
+private extension FoursquareApiManager {
+    struct FoursquareApi {
+        static let scheme = "https"
+        static let host = "api.foursquare.com"
+        static let path = "/v3"
+    }
+
+    func makeFoursquareApiComoponents() -> URLComponents {
+        var components = URLComponents()
+        components.scheme = FoursquareApi.scheme
+        components.host = FoursquareApi.host
+        components.path = FoursquareApi.path + "/places/search"
+
+        return components
+    }
+}
--- NicePlaceApp/NicePlaceAppTests/Service/FoursquareServiceTests.swift
+++ NicePlaceApp/NicePlaceAppTests/Service/FoursquareServiceTests.swift
@@ -5,4 +5,54 @@
 //  Created by Camila Luísa Farias on 21/08/22.
 //
 
-import Foundation
+import XCTest
+@testable import NicePlaceApp
+
+class GithubAPIManagerTests: XCTestCase {
+    var dispatchQueueMock: DispatchQueueWrapperProtocolMock!
+    var urlSessionMock: UrlSessionMock!
+    var sut: FoursquareApiManager!
+    
+    override func setUp() {
+        super.setUp()
+        dispatchQueueMock = DispatchQueueWrapperProtocolMock()
+        urlSessionMock = UrlSessionMock()
+        sut = FoursquareApiManager(session: urlSessionMock, dispatchQueueWrapper: dispatchQueueMock)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        dispatchQueueMock = nil
+        urlSessionMock = nil
+        sut = nil
+    }
+    
+    func test_placeSeach_withSuccess_shouldReturnFoursquareApiResponse() {
+        //given
+        let foursquareApiResponse = FoursquareApiResponse(results: [])
+        urlSessionMock.mockResponse = getMockFoursquareApiResponse(with: foursquareApiResponse)
+        var checkFoursquareApiResponse: FoursquareApiResponse?
+        
+        //When
+        sut.placeSeach(placeName: "", latitude: 0, longitude: 0) { result in
+            switch result {
+            case .success(let reponse):
+                checkFoursquareApiResponse = reponse
+            case .failure:
+                break
+            }
+        }
+        
+        //Then
+        XCTAssertNotNil(checkFoursquareApiResponse)
+        XCTAssertEqual(foursquareApiResponse, checkFoursquareApiResponse)
+    }
+    
+    private func getMockFoursquareApiResponse(with response: FoursquareApiResponse) -> Data? {
+        let encoder = JSONEncoder()
+        encoder.keyEncodingStrategy = .convertToSnakeCase
+        let data = try? encoder.encode(response)
+
+        return data
+    }
+}
