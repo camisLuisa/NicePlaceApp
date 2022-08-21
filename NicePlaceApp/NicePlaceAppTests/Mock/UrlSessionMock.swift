c9607038bebf2965ab5ba27ea5677fb3a168450e
Camila Luísa Farias <camila.luisafarias@thoughtworks.com>
2022-08-21 15:11:29 -0300

    Setup UrlSessionMock

--- NicePlaceApp/NicePlaceAppTests/Mock/UrlSessionMock.swift
+++ NicePlaceApp/NicePlaceAppTests/Mock/UrlSessionMock.swift
@@ -5,4 +5,27 @@
 //  Created by Camila Luísa Farias on 21/08/22.
 //
 
-import Foundation
+import XCTest
+@testable import NicePlaceApp
+
+class UrlSessionMock: URLSessionProtocol {
+    var mockResponse: Data?
+    var error: NetworkError?
+
+    func dataTask(
+        urlRequest: URLRequest,
+        completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol {
+        if let mockResponse = mockResponse {
+            completionHandler(
+                mockResponse, HTTPURLResponse(
+                    url: URL(string: "google.com")!,
+                    statusCode: 200,
+                    httpVersion: nil,
+                    headerFields: nil),
+                nil)
+        } else if let error = error {
+            completionHandler(nil, nil, error)
+        }
+        return URLSessionDataTaskProtocolMock()
+    }
+}
