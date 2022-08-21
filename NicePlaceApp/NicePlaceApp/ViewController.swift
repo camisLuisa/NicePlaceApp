295f6f53d44b5bbd8d3e90d5f4ae73dcb6067ca2
Camila Luísa Farias <camila.luisafarias@thoughtworks.com>
2022-08-21 14:01:21 -0300

    Create NicePlaceApp

---
+++ NicePlaceApp/NicePlaceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NicePlaceApp
+//
+//  Created by Camila Luísa Farias on 21/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
