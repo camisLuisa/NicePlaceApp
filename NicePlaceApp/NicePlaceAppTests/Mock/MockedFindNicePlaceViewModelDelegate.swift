140fe331676fc3280dd80fe28acc6691e6c0d2b1
Camila Luísa Farias <camila.luisafarias@thoughtworks.com>
2022-08-21 16:16:02 -0300

    Setup MockedFindNicePlaceViewModelDelegate

--- NicePlaceApp/NicePlaceAppTests/Mock/MockedFindNicePlaceViewModelDelegate.swift
+++ NicePlaceApp/NicePlaceAppTests/Mock/MockedFindNicePlaceViewModelDelegate.swift
@@ -5,4 +5,25 @@
 //  Created by Camila Luísa Farias on 21/08/22.
 //
 
-import Foundation
+import XCTest
+@testable import NicePlaceApp
+
+class MockedFindNicePlaceViewModelDelegate: FindNicePlaceViewModelDelegate {
+    var invokedGetPlaces = false
+    var invokedShowError = false
+    var invokedShowloading = false
+    
+    func getPlaces(places: [PlaceModel]) {
+        invokedGetPlaces = true
+    }
+    
+    func showError() {
+        invokedShowError = true
+    }
+    
+    func showLoading() {
+        invokedShowloading = true
+    }
+    
+    
+}
