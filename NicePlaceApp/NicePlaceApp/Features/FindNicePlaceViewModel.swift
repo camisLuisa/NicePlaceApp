4d757be9cdb1caed62807ed903f65dfde0c4a35e
Camila Luísa Farias <camila.luisafarias@thoughtworks.com>
2022-08-21 16:17:40 -0300

    Setup FindNicePlaceViewModel

--- NicePlaceApp/NicePlaceApp/Features/FindNicePlaceViewModel.swift
+++ NicePlaceApp/NicePlaceApp/Features/FindNicePlaceViewModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+protocol FindNicePlaceViewModelDelegate: AnyObject {
+    func getPlaces(places: [PlaceModel])
+    func showError()
+    func showLoading()
+}
+
+protocol FindNicePlaceViewModelProtocol {
+    var delegate: FindNicePlaceViewModelDelegate? { get set }
+    func findPlaces(with name: String, latitude: Float, longitude: Float)
+}
+
+class FindNicePlaceViewModel {
+    let service: FoursquareApiManagerProtocol
+    weak var delegate: FindNicePlaceViewModelDelegate?
+    
+    init(service: FoursquareApiManagerProtocol = FoursquareApiManager()) {
+        self.service = service
+    }
+    
+    func findPlaces(with name: String, latitude: Float, longitude: Float) {
+        delegate?.showLoading()
+        service.placeSeach(placeName: name, latitude: latitude, longitude: latitude) { response in
+            switch response {
+            case .success(let places):
+                let placesResponse = places.results.map { foursquareResult in
+                    PlaceModel(with: foursquareResult)
+                }
+                
+                self.delegate?.getPlaces(places: placesResponse)
+            case .failure:
+                self.delegate?.showError()
+            }
+        }
+    }
+}
