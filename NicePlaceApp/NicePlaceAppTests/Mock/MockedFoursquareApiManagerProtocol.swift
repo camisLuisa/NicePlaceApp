55161fdd70c07a987bc2ff4d118bf135f263ec51
Camila Luísa Farias <camila.luisafarias@thoughtworks.com>
2022-08-21 16:16:27 -0300

    Setup FindNicePlaceViewModelTests

--- NicePlaceApp/NicePlaceAppTests/Mock/MockedFoursquareApiManagerProtocol.swift
+++ NicePlaceApp/NicePlaceAppTests/Mock/MockedFoursquareApiManagerProtocol.swift
@@ -5,4 +5,23 @@
 //  Created by Camila Luísa Farias on 21/08/22.
 //
 
-import Foundation
+import XCTest
+@testable import NicePlaceApp
+
+class MockedFoursquareApiManagerProtocol: FoursquareApiManagerProtocol {
+    var mockedResponse = FoursquareApiResponse(results: [])
+    var mockedError: NetworkError?
+    
+    func placeSeach(placeName: String, latitude: Float, longitude: Float, completion: @escaping (Result<FoursquareApiResponse, NetworkError>) -> Void) {
+        
+        if let mockedError = mockedError {
+            completion(.failure(mockedError))
+            
+            return
+        }
+        
+        completion(.success(mockedResponse))
+    }
+    
+    
+}
