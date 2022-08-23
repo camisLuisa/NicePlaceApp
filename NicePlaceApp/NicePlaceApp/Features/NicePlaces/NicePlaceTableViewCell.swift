f9c507793b83a44bf66216c69e74146a8598e7f9
Camila Luísa Farias <camila.luisafarias@thoughtworks.com>
2022-08-22 22:50:00 -0300

    Create NicePlaceTableViewCell

---
+++ NicePlaceApp/NicePlaceApp/Features/NicePlaces/NicePlaceTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NicePlaceTableViewCell.swift
+//  NicePlaceApp
+//
+//  Created by Camila Luísa Farias on 22/08/22.
+//
+
+import UIKit
+
+class NicePlaceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
