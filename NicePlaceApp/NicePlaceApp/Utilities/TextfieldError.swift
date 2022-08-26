eb4a74c35439e5a1b2709023a0cb4cdececb9550
Camila Luísa Farias <camila.luisafarias@thoughtworks.com>
2022-08-25 21:10:25 -0300

    Create TextfieldError

---
+++ NicePlaceApp/NicePlaceApp/Utilities/TextfieldError.swift
@@ -0,0 +1,12 @@
+//
+//  TextfieldError.swift
+//  NicePlaceApp
+//
+//  Created by Camila Luísa Farias on 25/08/22.
+//
+
+import Foundation
+
+enum TextfieldError: Error {
+    case emptyTextfield
+}
