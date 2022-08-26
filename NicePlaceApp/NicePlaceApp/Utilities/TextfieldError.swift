5c2101a65a9ebaa683021368bdedb0e55e1ddf0b
Camila Luísa Farias <camila.luisafarias@thoughtworks.com>
2022-08-25 22:01:56 -0300

    Create messageError

--- NicePlaceApp/NicePlaceApp/Utilities/TextfieldError.swift
+++ NicePlaceApp/NicePlaceApp/Utilities/TextfieldError.swift
@@ -9,4 +9,14 @@ import Foundation
 
 enum TextfieldError: Error {
     case emptyTextfield
+    case outOfRadiusValueLimit
+    
+    var messageError: String {
+        switch self {
+        case .emptyTextfield:
+            return "Category, latitude and longitude cannot be empty."
+        case .outOfRadiusValueLimit:
+            return "Radius must be between 0 and 100000."
+        }
+    }
 }
